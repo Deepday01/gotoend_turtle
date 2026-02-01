07a51da72dd6a65c80785654f567bc2266c9aa3a
SongyeongMan <thsrudaks6334@gmail.com>
2026-02-01 21:25:20 +0900

    modified:action, pub

---
+++ rokey_ws/src/hong_pkg/hong_pkg/modules/drive_commander.py
@@ -0,0 +1,22 @@
+from geometry_msgs.msg import Twist
+
+class DriveCommander:
+    def __init__(self, node):
+        self.node = node
+        self.publisher = node.create_publisher(Twist, '/robot4/cmd_vel', 10)
+
+    def send_velocity(self, linear_x, angular_z):
+        msg = Twist()
+        
+        msg.linear.x = float(linear_x)
+        msg.linear.y = 0.0              
+        msg.linear.z = 0.0              
+
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = float(angular_z)
+
+        self.publisher.publish(msg)
+
+    def stop(self):
+        self.send_velocity(0.0, 0.0)
