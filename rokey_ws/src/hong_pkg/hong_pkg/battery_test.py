5a6c96770ace0f21fdf23e333470d910e5e0d54c
Son Gyeongman <thsrudaks6334@gmail.com>
2026-02-03 12:03:46 +0900

    modifeid:test

--- rokey_ws/src/hong_pkg/hong_pkg/battery_test.py
+++ rokey_ws/src/hong_pkg/hong_pkg/battery_test.py
@@ -4,11 +4,13 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.duration import Duration
 from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy, HistoryPolicy
 from rclpy.time import Time
+from rclpy.qos import qos_profile_sensor_data
 
 import numpy as np
-import threading
+from threading import lock, Thread
 import time
 import math
+from sensor_msgs.msg import BatteryState
 
 from enum import Enum, auto
 
@@ -24,7 +26,7 @@ class Batterytest(Node):
     def __init__(self):
         super().__init__('depth_to_map_node')
 
-        self.lock = threading.Lock()
+        self.lock = lock
 
         # 변수 초기화
         self.state = RobotState.START
@@ -32,9 +34,13 @@ class Batterytest(Node):
         ns = self.get_namespace()
         self.depth_topic = f'{ns}/oakd/stereo/image_raw'
 
-        self.create_subscription(CameraInfo, self.info_topic, self.camera_info_callback, 1)
+        self.battery_state_subscriber = self.create_subscription(
+            BatteryState,
+            'battery_state',
+            self.battery_state_callback,
+            qos_profile_sensor_data)
 
-    def process_loop(self):
+    def battery_state_callback(self):
         pass
 
 def main():
